05672f0a3f7c9b06ec24b0388bbd99a9f9d1dac6
John Resig <jeresig@gmail.com>
2014-02-24 22:15:13 -0500

    Re-work names to be consistent with the new models.

--- ukiyoe.js
+++ ukiyoe.js
@@ -2,15 +2,17 @@ var ukiyoe = require("ukiyoe-models");
 
 ukiyoe.init(function() {
     require("stack-scraper").cli(function(args) {
+        if (!ukiyoe.models[args.type]) {
+            console.error("Error: Invalid model type:", args.type);
+            process.exit(1);
+        }
+
         return {
             rootDataDir: __dirname + "/../ukiyoe-search/data/",
             scrapersDir: __dirname + "/scrapers/",
-            model: ukiyoe.db.model(({
-                images: "ExtractedImage",
-                artists: "Bio"
-            })[args.type]),
+            model: ukiyoe.models[args.type],
             postProcessors: require("./processing/" + args.type)(ukiyoe),
-            directories: args.type === "images" ?
+            directories: args.type === "extractedimage" ?
                 ["./images/", "./thumbs/", "./scaled/"] : []
         };
     }, function(err) {
