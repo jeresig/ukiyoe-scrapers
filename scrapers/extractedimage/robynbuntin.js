069e810d8da18ac74f0368f7d9c739a83b8f8131
John Resig <jeresig@gmail.com>
2014-03-11 00:16:35 -0400

    Add Robyn Buntin of Honolulu scraper.

---
+++ scrapers/extractedimage/robynbuntin.js
@@ -0,0 +1,30 @@
+module.exports = function(options, casper) {
+    return {
+        scrape: [
+            {
+                start: "http://www.robynbuntin.com/Japanese-Prints-s/4012.htm",
+                visit: "//a[.//img][contains(@href,'product-p')]",
+                next: "(//input[contains(@src,'nextpage')])[1]"
+            },
+            {
+                extract: {
+                    title: ["//span[@itemprop='name']", function(val, data) {
+                        var parts = val.split(/\s+by\s+/);
+
+                        if (parts.length > 1) {
+                            data.artists = [parts[1]];
+                        }
+
+                        return parts[0];
+                    }],
+                    description: "//*[@id='descript-text']",
+                    dateCreated: "//*[@id='age-value']",
+                    dimensions: "//*[@id='size-value']",
+                    "images[]": ["//img[contains(@class,'product_photo')]/@src", function(val) {
+                        return "http:" + val.replace(/\?.*$/, "");
+                    }]
+                }
+            }
+        ]
+    };
+};
