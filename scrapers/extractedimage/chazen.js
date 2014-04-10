4fcb66f88f36f8b9dd21540b8c0865922b973b8e
John Resig <jeresig@gmail.com>
2014-04-10 18:41:56 -0400

    Better handle some oddly-named URLs.

--- scrapers/extractedimage/chazen.js
+++ scrapers/extractedimage/chazen.js
@@ -1,4 +1,4 @@
-// http://www.cmoa.org/CollectionDetail.aspx?item=1027993
+// http://embarkkiosk.chazen.wisc.edu/OBJ1097?sid=1234&x=194&port=1597
 
 module.exports = function(options, casper) {
     return {
@@ -12,8 +12,11 @@ module.exports = function(options, casper) {
                     artists: "//a[contains(@href,'Art')]",
                     dimensions: "//div[@id='objectDetailCell']/text()[contains(.,'mm')]",
                     "images[]": ["//img[contains(@src,'Previews')]/@src", function(val, data) {
+                        var val = val.replace(/Previews/, "images")
+                            .replace(/JPRINTS\/[^\/]+\/(\d+)-(.*.jpg)/,
+                            "Print_Room/processed/web_size/2010_08/19$1_$2");
                         data._ids = [/([^\/]+).jpg$/.exec(val)[1]];
-                        return val.replace(/Previews/, "images");
+                        return val;
                     }],
                     url: function(data) {
                         return data.url.replace(/\?.*$/, "");
